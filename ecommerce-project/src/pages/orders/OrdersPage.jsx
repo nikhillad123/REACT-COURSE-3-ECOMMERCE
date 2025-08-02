f230cac0626b31ab6e10947478191218a3b663dc
nikhillad123 <nikhil.lad24india@gmail.com>
2025-08-02 12:50:32 +0530

    Exercise 7-a,b,c

--- ecommerce-project/src/pages/orders/OrdersPage.jsx
+++ ecommerce-project/src/pages/orders/OrdersPage.jsx
@@ -11,11 +11,22 @@ import './OrdersPage.css'
 export function OrdersPage({ cart }) {
     const [orders, setOrders] = useState([]);
 
+    /*
     useEffect(() => {
         axios.get('/api/orders?expand=products')
             .then((response) => {
                 setOrders(response.data);
             });
+    }, []);
+    */
+
+    useEffect(() => {
+        const getOrderData = async () => {
+            const response = await axios.get('/api/orders?expand=products');
+            setOrders(response.data);
+        };
+
+        getOrderData();
     }, []);
 
     return (
