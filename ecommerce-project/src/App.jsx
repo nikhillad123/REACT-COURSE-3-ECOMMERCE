19674aae9346c524a5c5b88e187fad4dae3249b9
nikhillad123 <nikhil.lad24india@gmail.com>
2025-08-02 16:54:25 +0530

    Exercise 7-i, j, k

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -34,7 +34,7 @@ function App() {
       <Route index element={<HomePage cart={cart} />} />
       <Route path="checkout" element={<CheckoutPage cart={cart} />} />
       <Route path="orders" element={<OrdersPage cart={cart} />} />
-      <Route path="tracking" element={<TrackingPage cart={cart} />} />
+      <Route path="tracking/:orderId/:productId" element={<TrackingPage cart={cart} />} />
       <Route path="*" element={<ErrorPage cart={cart} />} />
     </Routes>
   )
--- ecommerce-project/src/pages/trackings/TrackingPage.jsx
+++ ecommerce-project/src/pages/trackings/TrackingPage.jsx
@@ -1,10 +1,35 @@
+import { useParams } from 'react-router'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import dayjs from 'dayjs'
 import { Link } from 'react-router'
 import { Header } from '../../components/Header';
 import './TrackingPage.css'
 
 export function TrackingPage({ cart }) {
+    const { orderId, productId } = useParams();
+    const [order, setOrder] = useState(null);
+
+    useEffect(() => {
+        const fetchTrackingData = async () => {
+            const response = await axios.get(`/api/orders/${orderId}?expand=products`);
+            setOrder(response.data);
+        };
+
+        fetchTrackingData();
+    }, [orderId]);
+
+    if (!order) {
+        return null;
+    }
+
+    const orderProduct = order.products.find((orderProduct) => {
+        return orderProduct.productId === productId;
+    });
+
     return (
         <>
+            <title>Tracking</title>
             <link rel="icon" type="image/svg+xml" href="./tracking-favicon.png" />
 
             <Header cart={cart} />
@@ -16,18 +41,18 @@ export function TrackingPage({ cart }) {
                     </Link>
 
                     <div className="delivery-date">
-                        Arriving on Monday, June 13
+                        Arriving on {dayjs(orderProduct.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
                     </div>
 
                     <div className="product-info">
-                        Black and Gray Athletic Cotton Socks - 6 Pairs
+                        {orderProduct.product.name}
                     </div>
 
                     <div className="product-info">
-                        Quantity: 1
+                        Quantity: {orderProduct.quantity}
                     </div>
 
-                    <img className="product-image" src="images/products/athletic-cotton-socks-6-pairs.jpg" />
+                    <img className="product-image" src={orderProduct.product.image} />
 
                     <div className="progress-labels-container">
                         <div className="progress-label">
